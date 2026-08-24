73dd41ef955313ca67a3e646816a00759f8bf386
jampalasaideepika <jampalasaideepika@gmail.com>
2026-08-24 10:42:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-let boxes=document.querySelector(".container").children
-function getRandomColor(){
-    let val1=Math.ceil(0+Math.random()*255);
-    let val2=Math.ceil(0+Math.random()*255);
-    let val3=Math.ceil(0+Math.random()*255);
-    return`rgb(${val1},${val1},${val3})`
-}
-Array.from(boxes).forEach((e)=>{
-    e.style.backgroundColor=getRandomColor(); 
-    e.style.color=getRandomColor();
+console.log("He is hacker")
+console.log("She is the real hacker")
 
-})
+setTimeout(()=>{
+    console.log("I am inside set time out");
+},2000);
--- script2.js
+++ script2.js
@@ -1,10 +1,1 @@
-let button=document.getElementById("btn")
-button.addEventListener("click",()=>{
-    document.querySelector(".box").innerHTML="<b>I have created successfully</b>"
-})
-button.addEventListener("click",()=>{
-    alert("Dont hack us by right click please")
-})
-document.addEventListener("keydown",e=>{
-    console.log(e, e.key,e.keyCode)
-})
+console.log("He is a hacker")
