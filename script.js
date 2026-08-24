d8b5b197ac259cf45fe329e23f0bd50930d2d840
jampalasaideepika <jampalasaideepika@gmail.com>
2026-08-24 10:40:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,78 @@
-function mean(a,b,c,d,e){
-    console.log((a+b+c+d+e)/5);
+// let arr =[1,2,3,4]
+// let n=arr.length
+// for( let i=arr.length-1;i>=0;i--){
+// console.log(arr[i]*arr[i])
+// }
+
+
+// let arr =[5,4,1,3,2,5,7,9,6,3]
+// let n=arr.length
+// for(let i=1;i<=n;i+=2){
+//     console.log(arr[i]*arr[i])
+// }
+
+// let arr =[1,2,3,4,5]
+// let n=arr.length
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]+1)
+// }
+
+// let arr =[1,2,3,4,5]
+// let n=arr.length
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]*arr[i])
+// }
+
+
+
+// let b="*"
+// for(i=0;i<=5;i++){
+//     console.log(b.repeat(i));
+// }
+
+
+
+// var myarray=[1,2,3,4,5]
+// myarray.forEach(function(element){
+//     console.log(element);
+// });
+
+
+// var myarray=[1,2,3,4,5];
+// for( var element of myarray){
+//     console.log(element);
+// }
+
+
+
+
+// arr=[1,2,3,4,5]
+//  let arr2= arr.map(function (i){
+//     return i**2;
+// })
+//  console.log(arr2);
+
+
+// let arr3=arr.filter(function(i){
+//     return i%2===0
+// })
+// console.log(arr3);
+// arr=[1,2,3,4,5]
+// let sum=arr.reduce(function(a,c){
+//     return a+c;
+// },1);
+// console.log(sum)
+
+arr=[1,2,3,4,5]
+let sum2=(a,b)=>{
+    return a+b;
 }
-mean(1,2,3,4,5)
+console.log(arr.reduce(sum2))
 
-const mean2=(a,b,c,d,e)=>{
-    return(a+b+c+d+e)/5;
+let n=("5,4,7,5,2,3")
+for()
+fact=i
+for(i=1;i<=n;i++){
+    fact=fact*i;
 }
-mean2
-const square =(x)=>{
-    return x*x;
-}
-let a=square(10);
-console.log(a);
-
-const square2 =x => x*x;
-
-
-function outer(){
-    console.log("outer function running...");
-    function inner(){
-        console.log("inner function running...");
-    }
-    inner();
-
-}
-outer();
+console.log(fact of )
