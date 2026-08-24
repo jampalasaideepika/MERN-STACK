1b8957c0b888e16dfb1fe97e75ba0b2d5ff7d569
jampalasaideepika <jampalasaideepika@gmail.com>
2026-08-24 10:33:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,90 @@
-console.log("He is hacker")
-console.log("She is the real hacker")
+// console.log("This is my looping function");
+// let a=1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
 
-setTimeout(()=>{
-    console.log("I am inside set time out");
-},2000);
+
+// for (let i=1;i<=100;i++){
+//     console.log(i)
+// }
+
+// let obj={
+//     name:"deepu",
+//     roolno:30,
+//     branch:"csc"
+// }
+// for (const i in obj) { 
+//     const element = obj[i];
+    
+//     console.log(i);
+//     console.log(obj[i]);
+//     console.log(i,obj[i]);
+// }
+
+// for (const i of "hello") {
+//     console.log(i);
+// }
+// let i=1;
+// while (i<100){
+//     console.log("heyy!")
+//     i++;
+// }
+
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while (i <= 5);
+
+let a=10;
+let b=20;
+let max =(a>b)? a:b;
+console.log ("maximum numcer is :"+ max);
+
+let x="guava";
+switch (x){
+case "banana":
+    console.log("This is a guava");
+    break;
+    case "apple":
+        console.log("This is a guava");
+        break;
+        default:
+            console.log("i dont need any fruits");
+}
+
+for (let i =0; i<=100; i++){
+    if (i % 8=== 0){
+        console.log(i);
+    }
+}
+function nice(name){
+console.log("Hey " +name+ " your nice1");
+console.log("Hey " +name+ " your nice2");
+console.log("Hey " +name+ " your nice3");
+console.log("Hey " +name+ " your nice4");
+    
+
+}
+nice("sam");
+
+function sum(a,b){
+    //
+    return a+b;
+}
+total=sum(2,3);
+console.log(total);
+function sum2(a,b,c=3){
+    console.log(a+b+c);
+}
+sum2(1,2,3);
+sum2(1,2);
+
+const func1 =()=>{
+    console.log("I am an arrow function",)
+}
+func1("Hello");
+
+
+
---
+++ task.js
@@ -0,0 +1,28 @@
+// let text = "HELLO";
+// let num = 10;
+// let result = text + num;
+// console.log(result);
+
+// let name = "deepu";
+// console.log(typeof name);
+
+// let age = 15;
+// if (age >= 10 && age <= 20){
+//     console.log("age is between 10 and 20");
+// } else {
+//     console.log("age is not between 10 and 20");
+// }
+
+let num = 9;
+if (num % 3 === 0){
+    console.log("The number is divisible by 3");
+}else {
+    console.log("The number is not divisible by 3");
+}
+
+let age = 18;
+if (age >= 18){
+    console.log("Eligible to vote");
+}else{
+    console.log("not eligible to vote");
+}
