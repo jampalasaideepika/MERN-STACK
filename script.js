81a6b7997c71ce038f5f891b7b99ff5a9f0d806c
jampalasaideepika <jampalasaideepika@gmail.com>
2026-08-24 10:41:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,10 @@
-// let arr =[1,2,3,4]
-// let n=arr.length
-// for( let i=arr.length-1;i>=0;i--){
-// console.log(arr[i]*arr[i])
-// }
-
-
-// let arr =[5,4,1,3,2,5,7,9,6,3]
-// let n=arr.length
-// for(let i=1;i<=n;i+=2){
-//     console.log(arr[i]*arr[i])
-// }
-
-// let arr =[1,2,3,4,5]
-// let n=arr.length
-// for(let i=arr.length-1;i>=0;i--){
-//     console.log(arr[i]+1)
-// }
-
-// let arr =[1,2,3,4,5]
-// let n=arr.length
-// for(let i=arr.length-1;i>=0;i--){
-//     console.log(arr[i]*arr[i])
-// }
-
-
-
-// let b="*"
-// for(i=0;i<=5;i++){
-//     console.log(b.repeat(i));
-// }
-
-
-
-// var myarray=[1,2,3,4,5]
-// myarray.forEach(function(element){
-//     console.log(element);
-// });
-
-
-// var myarray=[1,2,3,4,5];
-// for( var element of myarray){
-//     console.log(element);
-// }
-
-
-
-
-// arr=[1,2,3,4,5]
-//  let arr2= arr.map(function (i){
-//     return i**2;
-// })
-//  console.log(arr2);
-
-
-// let arr3=arr.filter(function(i){
-//     return i%2===0
-// })
-// console.log(arr3);
-// arr=[1,2,3,4,5]
-// let sum=arr.reduce(function(a,c){
-//     return a+c;
-// },1);
-// console.log(sum)
-
-arr=[1,2,3,4,5]
-let sum2=(a,b)=>{
-    return a+b;
-}
-console.log(arr.reduce(sum2))
-
-let n=("5,4,7,5,2,3")
-for()
-fact=i
-for(i=1;i<=n;i++){
-    fact=fact*i;
-}
-console.log(fact of )
+let boxes=document.getElementsByClassName("box");
+console.log(boxes)
+boxes[2].style.backgroundColor="aqua";
+document.getElementById("lightgreen") .style.backgroundColor="lightgreen";
+document.querySelector(".box").style.backgroundColor="royalblue";
+console.log(document.querySelectorAll(".box"));    
+document.querySelectorAll(".box")[1].style.backgroundColor="grey";
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="lightyellow";
+})
